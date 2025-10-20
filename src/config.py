d25dd236c9579333b3c936dc3fc1177be1d8bcbb
Tyby Boiciuc <tybyboiciuc@MacBook-Pro-Tyby.local>
2025-10-20 17:12:05 +0300

    aws config

--- src/config.py
+++ src/config.py
@@ -5,16 +5,24 @@ load_dotenv()
 
 class Config:
     PDF_PATH = os.getenv("PDF_PATH")
-    RAG_MODEL = os.getenv("RAG_MODEL")
-    RAG_MODEL_ID = os.getenv("RAG_MODEL_ID")
     SENTS_PER_CHUNK = int(os.getenv("SENTS_PER_CHUNK"))
-    SENT_OVERLAP = int(os.getenv("SENT_OVERLAP"))   
-    MAX_TOKENS = int(os.getenv("MAX_TOKENS"))
-    MIN_TOKENS = int(os.getenv("MIN_TOKENS"))
+    SENT_OVERLAP = int(os.getenv("SENT_OVERLAP"))
+    
+    # User's preferred embedding model
     EMBED_MODEL = os.getenv("EMBED_MODEL")
+
+    # AWS and Bedrock Settings
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_REGION = os.getenv("AWS_REGION")
+    # Switch the model to Mistral 7B Instruct
+    BEDROCK_MODEL_ID = "mistral.mistral-7b-instruct-v0:2"
+    MAX_TOKENS = int(os.getenv("MAX_TOKENS"))
+
     BATCH_SIZE = int(os.getenv("BATCH_SIZE"))
     DOC_ID = os.getenv("DOC_ID")
     BATCH_INSERT = int(os.getenv("BATCH_INSERT"))
+    TOP_K = int(os.getenv("TOP_K"))
     SUPABASE_URL = os.getenv("SUPABASE_URL")
     SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
